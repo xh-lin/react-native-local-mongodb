970d41f6c5dcd8b3988efe09583f4aab226d9714
xh-lin <xuhuanglin99@gmail.com>
2026-01-24 13:53:03 -0500

    fix persistence.test.js not picked up by jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testRegex: '(spec|test)\\.tsx?$',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testPathIgnorePatterns: [
+    "/test/dbTest.test.js$"
+  ],
   "moduleNameMapper": {
     "react-native": "<rootDir>/config/jest/reactNativeMock.js"
   },
