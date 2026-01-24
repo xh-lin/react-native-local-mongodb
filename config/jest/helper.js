ec3ff2575620446eb4e58478180bb69b48eafa89
Xu Huang Lin <55850802+xh-lin@users.noreply.github.com>
2026-01-24 14:25:13 -0500

    Set up repo for dev (#3)
    * gitignore package-lock.json
    
    * nvm use node ver 14
    
    * vscode settings tab size 2
    
    * fix persistence.test.js not picked up by jest
    
    * fix persistence.test.js
    
    * format on save

--- config/jest/helper.js
+++ config/jest/helper.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from './reactNativeMock'
 
 export const getDb = async () => {
   AsyncStorage.__reset()
-  const db = new DataStore({ filename: 'foo' })
+  const db = new DataStore({ filename: 'foo', storage: AsyncStorage })
   await db.loadDatabaseAsync()
   return db
 }
